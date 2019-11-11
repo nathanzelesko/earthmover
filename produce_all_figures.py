7d7d7cf6c80204d7b5eb4e55887a55c90745ac91
Amit Moscovich <amit@moscovich.org>
2019-11-11 11:26:53 -0500

    Added main script.

---
+++ produce_all_figures.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import earthmover_main
+
+def main():
+    print(' ==== Running all calculations ====')
+    earthmover_main.initial_calculations()
+
+    print(' ==== Proucing all figures ====')
+    earthmover_main.figure_all()
+
+
+if __name__ == '__main__':
+    main()
